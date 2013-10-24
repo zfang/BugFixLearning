63890ffd1e1487842b2314fceef16f827f5ecef0
Felix Fang <felixfangzh@gmail.com>
2013-10-24 15:59:53 -0400

    removed temp file

--- script/library_call_analysis/database/system_class.sql
+++
@@ -1,9 +0,0 @@
-CREATE TEMP TABLE library_call_analysis_dotnetlibraryclass_temp (
-   namespace character varying(255) NOT NULL,
-   classname character varying(255) NOT NULL,
-   function character varying(255) NOT NULL
-);
-
-\copy library_call_analysis_dotnetlibraryclass_temp FROM '/home/zfang/Documents/gendarme/BugFixLearning/script/library_call_analysis/dotnetclass/system_class.dat' WITH DELIMITER AS ' ';
-
-INSERT INTO library_call_analysis_dotnetlibraryclass (namespace, classname, function) SELECT * FROM library_call_analysis_dotnetlibraryclass_temp;
