db3ac72e148f733c742caf79894b173a1b8ba71d
Felix Fang <felixfangzh@gmail.com>
2013-11-01 04:07:50 -0400

    changed analyzer

--- script/library_call_analysis/database/analyze_function_calls.sql
+++ script/library_call_analysis/database/analyze_function_calls.sql
@@ -1,11 +1,10 @@
 CREATE OR REPLACE FUNCTION library_call_analysis_analyze_function_calls(_slug varchar) RETURNS 
 TABLE (
-   slug varchar(50),
-   sha1 varchar(40),
    patch_id int,
+   sha1 varchar(40),
    start_line_number int,
-   minus_code_line_num int,
-   plus_code_line_num int,
+   minus_line_num int,
+   plus_line_num int,
    minus_code text,
    plus_code text
 )
@@ -14,8 +13,8 @@ BEGIN
 
 RETURN QUERY
 SELECT 
-r.slug, p.sha1, 
-d1.patch_id, d1.start_line_number, 
+p.id, p.sha1, 
+d1.start_line_number, 
 d1.line_number AS minus_code_line_num, d2.line_number AS plus_code_line_num, 
 d1.code AS minus_code, d2.code AS plus_code 
 FROM 
@@ -29,11 +28,11 @@ AND d1.patch_id = d2.patch_id
 AND d1.start_line_number = d2.start_line_number
 AND d1.type='-' AND d2.type = '+'
 AND EXISTS 
-(SELECT 1 FROM library_call_analysis_dotnetlibraryclass_search_string s
-WHERE d1.code LIKE ('%' || s.string || '%') 
-AND d2.code LIKE ('%' || s.string || '%') )
---WHERE d1.code % (s.string) 
---AND d2.code % (s.string) )
+(
+   SELECT 1 FROM library_call_analysis_dotnetlibraryclass s
+   WHERE (d1.code LIKE ('%' || s.classname || '%') AND d2.code LIKE ('%' || s.classname || '%') )
+   OR (d1.code LIKE ('%' || s.function || '%') AND d2.code LIKE ('%' || s.function || '%') ) LIMIT 1
+)
 ;
 END
 $$ LANGUAGE plpgsql;
